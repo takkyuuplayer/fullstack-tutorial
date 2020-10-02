39dfe99b2fe4568011fe63c4a6eb4db5e2d93eb2
takkyuuplayer <takkyuuplayer@gmail.com>
2020-10-02 16:29:38 +0900

    8. useMutation

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,23 +1,34 @@
-  import {
-    ApolloClient,
-    InMemoryCache,
-    NormalizedCacheObject,
-    ApolloProvider
-  } from '@apollo/client';
-  import React from 'react';
-  import ReactDOM from 'react-dom';
-  import Pages from './pages';
-  import injectStyles from './styles';
-  
-  const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-    uri: 'http://localhost:4000/',
-    cache: new InMemoryCache()
-  });
-  
-  injectStyles();
-  ReactDOM.render(
-    <ApolloProvider client={client}>
-      <Pages />
-    </ApolloProvider>,
-    document.getElementById('root')
-  );
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import injectStyles from "./styles";
+
+const cache = new InMemoryCache();
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  uri: "http://localhost:4000/",
+  headers: {
+    authorization: localStorage.getItem("token")!,
+  },
+  cache,
+});
+// cache.writeData({
+//   data: {
+//     isLoggedIn: !!localStorage.getItem("token"),
+//     cartItems: [],
+//   },
+// });
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
